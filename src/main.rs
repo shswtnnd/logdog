b5488889407dfa65f52530f633cf4b83629e055b
Shashwat <shashwat1043@gmail.com>
2026-02-27 03:48:36 +0530

    error handaling in step and fetch command

--- src/main.rs
+++ src/main.rs
@@ -28,29 +28,31 @@ fn main() {
             file.write_all(b"# Vulnerability Report\n\n## Steps to Reproduce:\n").expect("Failed to write");
         }
         Commands::step{description}=>{
-            let mut file = OpenOptions::new()
-                .append(true)
-                .open("report.md")
-                .expect("Error: Could not find report.md. Did you run 'init' first?");
-            let formatted_step = format!("- {}\n", description);
+            let mut file = match OpenOptions::new().append(true).open("report.md") {
+                Ok(f)=> f,
+                Err(_)=>{
+                    eprint!("❌ Error: 'report.md' not found. Run 'logdog init' first!");
+                    return;
+                }
+            };
+            let formatted_step = format!("- {}\n",description);
             file.write_all(formatted_step.as_bytes()).expect("Failed to write step");
             println!("✅ Step logged: {}", description);
         }
         Commands::fetch => {
             let mut input_data = String::new();
             io::stdin().read_to_string(&mut input_data).expect("Failed to read terminal output");
-
             //Open the file to append
-            let mut file = OpenOptions::new()
-                .append(true)
-                .open("report.md")
-                .expect("Error: Could not find report.md. Try running logdog init");
-
+            let mut file = match OpenOptions::new().append(true).open("report.md"){
+                Ok(f)=>f,
+                Err(_)=>{
+                    eprintln!("❌ Error: 'report.md' not found. Run 'logdog init' first!");
+                    return;
+                }
+            };
             let formatted_code_block = format!("\n**Terminal Evidence:**\n```bash\n{}\n```\n", input_data.trim());
-
             //Write to the file
             file.write_all(formatted_code_block.as_bytes()).expect("Failed to write to report");
-            
             println!("LogDog fetched your terminal output.");
         }
     }
